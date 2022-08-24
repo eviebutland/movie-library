199b4e1f76569b4bd7fd7d30efce7490bed6ec62
EvieButland <evie.butland@hectare.farm>
2022-08-24 08:50:32 +0100

    trying to get server to run

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
-import OpenAPIBackend from "openapi-backend";
+import { OpenAPIBackend, OpenAPIRouter } from 'openapi-backend';
+const api = new OpenAPIBackend({
+    definition: './movies.yml',
+    strict: true,
+    quick: false,
+    validate: true,
+    ignoreTrailingSlashes: true,
+    // ajvOpts: { unknownFormats: true }, // ajv is validation
+})
 
-// Create api with the open API file
-const api = new OpenAPIBackend({definition: './movies.yml'})
+//   api.register({
+//     getListAllMovies:(c, req, res) => { 
+//         console.log(c)
+
+//         res.status(200).json({ result: 'ok' }
+//     )}
+//   })
+
+api.register('getListAllMovies', function (c, req, res) {
+    console.log(req)
+    return {
+        status: 200,
+        body: JSON.stringify(['pet1', 'pet2']),
+    }
+})
+
+api.init()
+
+const router = new OpenAPIRouter({
+    definition: './movies.yml',
+    apiRoot: '/',
+    ignoreTrailingSlashes: true,
+})
+
+
+
+const parsedRequest = router.parseRequest({
+method: 'GET',
+path: '/',
+headers: {
+    accept: 'application/json',
+    // cookie: 'token=abc123;path=/',
+    },
+}, router.getOperation('getListAllMovies'));
+  
+console.log(parsedRequest, 'get');
